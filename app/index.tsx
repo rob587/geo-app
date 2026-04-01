236e2ee7f31858e048c608f02f85d70ceffba1bd
rob587 <robertocamm02@yahoo.com>
2026-04-01 14:49:42 +0200

    feat: add expo-location dependency and implement location permissions in index

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,22 @@
+import * as Location from "expo-location";
+import { useState } from "react";
 import { Text, View } from "react-native";
 
+type Coords = {
+  latitude: number;
+  longitude: number;
+  accuracy: number | null;
+};
+
 export default function Index() {
+  // permesso di geo
+  const [permesso, richiediPermesso] = Location.useForegroundPermissions();
+  // gestioni di stato
+  const [coords, setCoords] = useState<Coords | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  if (!permesso) return <View />;
+
   return (
     <View
       style={{
